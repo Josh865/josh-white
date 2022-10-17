312618c5db6abd06b7196478156ee5330f0ed419
Josh <joshdwhite@gmail.com>
2022-10-17 17:25:28 -0400

    Style links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,15 +20,18 @@ import { VueLogo } from '@/components/logos/vue';
 import { TailwindLogo } from '@/components/logos/tailwind';
 import { ReactNativeLogo } from '@/components/logos/react-native';
 
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid';
+
 const projects = [
   {
     name: 'eeds Dashboard',
     year: '2022',
     description:
       'A prototype for a reimagined eeds administrative dashboard, featuring dynamic, interactive charts, an improved information hierarchy, and novel yet intuitive UI interactions to enable advanced functionality with minimal visual clutter.',
-    href: '#',
+    href: 'https://eeds-dashboard.vercel.app/',
     image: eedsDashboardImage,
     logos: [ReactLogo, NextLogo, TailwindLogo],
+    accentColor: 'border-[#7e22ce] group-hover:text-[#7e22ce]',
   },
   // {
   //   name: 'eeds Landing Page',
@@ -42,18 +45,20 @@ const projects = [
     year: '2021',
     description:
       'A new-from-the-ground-up rebuild of the aging website for Purdue’s Copyright Office. Highlights include an interactive fair use analysis, a clearer presentation of decision trees, and more effective use of color and weight to convey intent and importance.',
-    href: '#',
+    href: 'https://www.lib.purdue.edu/uco/',
     image: copyrightOfficeImage,
     logos: [LaravelLogo, VueLogo, TailwindLogo],
+    accentColor: 'border-[#8e6f3e] group-hover:text-[#8e6f3e]',
   },
   {
     name: 'Purdue COVID Stories',
     year: '2021',
     description:
       'This collaborative project between Purdue Libraries and Office of the Provost enables the Purdue and boader West Lafayette communities to share their experiences of the COVID-19 pandemic. The interactive form allows for a dynamic number of entries and stories can be sorted and filtered according to a number of criteria.',
-    href: '#',
+    href: 'https://webapps.lib.purdue.edu/covid-stories/',
     image: covidStoriesImage,
     logos: [LaravelLogo, VueLogo, TailwindLogo],
+    accentColor: 'border-[#8e6f3e] group-hover:text-[#8e6f3e]',
   },
 
   {
@@ -61,36 +66,40 @@ const projects = [
     year: '2020',
     description:
       'Purdue’s large archive of oral history recordings are made available for easy discovery and access through a robust search experience. A custom administrative interface allows staff to easily catalog and preserve large amounts of metadata for each oral history.',
-    href: '#',
+    href: 'https://collections.lib.purdue.edu/oral-history/',
     image: oralHistoriesImage,
     logos: [LaravelLogo, VueLogo, TailwindLogo],
+    accentColor: 'border-[#8e6f3e] group-hover:text-[#8e6f3e]',
   },
   {
     name: 'Wilmeth Active Learning Center',
     year: '2019',
     description:
       'This site showcases the Libraries’ most advanced space to date, allowing visitors to learn more about its history, facilities, and mission.',
-    href: '#',
+    href: 'https://www.lib.purdue.edu/walc/',
     image: walcImage,
     logos: [TailwindLogo],
+    accentColor: 'border-[#8e6f3e] group-hover:text-[#8e6f3e]',
   },
   {
     name: 'eeds Mobile Application',
     year: '2018',
     description:
       'A cross-platform mobile application written in React Native that allows attendees to medicial education events to easily document and manage their attendance.',
-    href: '#',
+    href: 'https://apps.apple.com/us/app/eeds-mobile/id541573257?platform=iphone',
     image: eedsAppImage,
     logos: [ReactNativeLogo],
+    accentColor: 'border-[#3465fd] group-hover:text-[#3465fd]',
   },
   {
     name: 'Purdue Campus History',
     year: '2017',
     description:
-      'This multi-year, grant-funded project allows visitors to explore the history of Purdue’s West Lafayette campus through an interactive map built with Esri’s ArcGIS JavaScript Api, a full-featured search experience built with Algolia, and a dynamic building timeline that allows visitors to explore how the campus’s buildings have changed over its history.',
-    href: '#',
+      'This multi-year, grant-funded project allows visitors to explore the history of Purdue’s West Lafayette campus through an interactive map built with Esri’s ArcGIS JavaScript Api, a full-featured search experience built with Algolia, and a dynamic building timeline that allows visitors to explore how the campus’s buildings have changed over its history. It was featured in local and national news outlets, including the Houston Chronicle, San Antonio Express-News, and Albany (NY) Times Union, among others.',
+    href: 'http://collections.lib.purdue.edu/campus/',
     image: campusDatabaseImage,
     logos: [LaravelLogo, VueLogo, TailwindLogo],
+    accentColor: 'border-[#8e6f3e] group-hover:text-[#8e6f3e]',
   },
 ];
 
@@ -160,7 +169,24 @@ export default function Home() {
                     <div className="grid grid-cols-1 gap-8 py-12 md:grid-cols-[33%,1fr]">
                       <div className="flex flex-col">
                         <h3 className="text-lg font-medium tracking-tight text-zinc-800 sm:text-xl sm:font-bold">
-                          {project.name}
+                          <div className="">
+                            <a
+                              href={project.href}
+                              target="_blank"
+                              rel="noreferrer"
+                              className={`group inline-flex items-center gap-x-1`}
+                            >
+                              <span
+                                className={`border-b-2 transition ${project.accentColor}`}
+                              >
+                                {project.name}
+                              </span>
+                              <ArrowTopRightOnSquareIcon
+                                className="h-4 w-4 text-zinc-500 opacity-0 transition group-hover:opacity-100"
+                                aria-hidden="true"
+                              />
+                            </a>
+                          </div>
                         </h3>
                         <p className="order-first text-sm font-bold tracking-tight text-zinc-400">
                           {project.year}
@@ -179,16 +205,12 @@ export default function Home() {
                           </div>
                         ) : null}
                       </div>
-                      <div className="">
-                        <Link href="/" aria-label="Home">
-                          <Image
-                            src={project.image}
-                            alt=""
-                            className="h-full w-full rounded-xl bg-zinc-100 object-cover shadow-lg dark:bg-zinc-800"
-                            priority
-                          />
-                        </Link>
-                      </div>
+                      <Image
+                        src={project.image}
+                        alt={`Screenshots of the ${project.name} application`}
+                        className="h-full w-full rounded-xl bg-zinc-100 object-cover shadow-lg"
+                        priority
+                      />
                     </div>
                   </div>
                 ))}
